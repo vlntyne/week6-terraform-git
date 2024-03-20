463d78e4773e37a916665f9407b055a27093532f
vlntyne <francis1daz@gmail.com>
2024-03-20 17:46:58 -0400

    terraform code

---
+++ code3/lightsail.tf
@@ -0,0 +1,15 @@
+
+resource "aws_lightsail_instance" "custom" {
+  name              = "my-apache"
+  availability_zone = "us-east-1b"
+  blueprint_id      = "amazon_linux_2"
+  bundle_id         = "nano_1_0"
+  user_data         = "sudo yum install -y httpd && sudo systemctl start httpd && sudo systemctl enable httpd && echo '<h1>Deployed via Terraform</h1>' | sudo tee /var/www/html/index.html"
+}
+
+output "instance_ip" {
+  value = aws_lightsail_instance.custom.public_ip_address 
+}
+output "my-arn" {
+    value = aws_lightsail_instance.custom.arn
+}
